d4c4cf7d312568f0efa26961ed37146f2c4dae1e
j0hun <dudgns3681@gmail.com>
2024-03-05 17:04:46 +0900

    8장 게시글 삭제하기: Delete

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -65,5 +66,15 @@ public class MemberController {
         model.addAttribute("member",member);
         return "redirect:/members/" + member.getId();
 
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes redirectAttributes){
+        Member member = memberRepository.findById(id).orElse(null);
+        if(member != null){
+            memberRepository.delete(member);
+            redirectAttributes.addFlashAttribute("msg","삭제됐습니다!");
+        }
+        return "redirect:/members";
     }
 }
