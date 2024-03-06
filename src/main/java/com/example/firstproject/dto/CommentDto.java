748a042cb4bf60a25e84e36ee448639cca0d9448
j0hun <dudgns3681@gmail.com>
2024-03-06 16:22:52 +0900

    18장 웹페이지에서 댓글 수정하기

--- src/main/java/com/example/firstproject/dto/CommentDto.java
+++ src/main/java/com/example/firstproject/dto/CommentDto.java
@@ -1,7 +1,6 @@
 package com.example.firstproject.dto;
 
 import com.example.firstproject.entity.Comment;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,7 +14,6 @@ public class CommentDto {
 
 
     private Long id;
-    @JsonProperty("article_id")
     private Long articleId;
     private String nickname;
     private String body;
